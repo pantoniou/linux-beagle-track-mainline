28c5a7118a3b8d5834c1f304acf8932ff3303e11
Pantelis Antoniou <pantelis.antoniou@konsulko.com>
2014-06-12 22:31:23 +0300

    configfs: Implement binary attributes
    Some problems require the use of binary configfs attributes, so
    this patch implements them.
    
    Signed-off-by: Pantelis Antoniou <pantelis.antoniou@konsulko.com>

--- fs/configfs/dir.c
+++ fs/configfs/dir.c
@@ -257,6 +257,13 @@ static int configfs_init_file(struct inode * inode)
 	return 0;
 }
 
+static int configfs_init_bin_file(struct inode * inode)
+{
+	inode->i_size = 0;
+	inode->i_fop = &configfs_bin_file_operations;
+	return 0;
+}
+
 static int init_symlink(struct inode * inode)
 {
 	inode->i_op = &configfs_symlink_inode_operations;
@@ -431,7 +438,9 @@ static int configfs_attach_attr(struct configfs_dirent * sd, struct dentry * den
 	spin_unlock(&configfs_dirent_lock);
 
 	error = configfs_create(dentry, (attr->ca_mode & S_IALLUGO) | S_IFREG,
-				configfs_init_file);
+				(sd->s_type & CONFIGFS_ITEM_ATTR) ?
+					configfs_init_file :
+					configfs_init_bin_file);
 	if (error) {
 		configfs_put(sd);
 		return error;
@@ -591,6 +600,7 @@ static int populate_attrs(struct config_item *item)
 {
 	struct config_item_type *t = item->ci_type;
 	struct configfs_attribute *attr;
+	struct configfs_bin_attribute *bin_attr;
 	int error = 0;
 	int i;
 
@@ -599,6 +609,13 @@ static int populate_attrs(struct config_item *item)
 	if (t->ct_attrs) {
 		for (i = 0; (attr = t->ct_attrs[i]) != NULL; i++) {
 			if ((error = configfs_create_file(item, attr)))
+				break;
+		}
+	}
+
+	if (t->ct_bin_attrs) {
+		for (i = 0; (bin_attr = t->ct_bin_attrs[i]) != NULL; i++) {
+			if ((error = configfs_create_bin_file(item, bin_attr)))
 				break;
 		}
 	}
