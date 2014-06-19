2867b6e281c7241e01ddafdccc0dc4230129e06d
Pantelis Antoniou <pantelis.antoniou@konsulko.com>
2014-06-19 16:48:48 +0300

    of: Introduce tree change __foo_post methods.
    As part of the DT transactional support these need to be introduced.
    
    Signed-off-by: Pantelis Antoniou <pantelis.antoniou@konsulko.com>

--- include/linux/of.h
+++ include/linux/of.h
@@ -339,9 +339,14 @@ extern int of_alias_get_id(struct device_node *np, const char *stem);
 extern int of_machine_is_compatible(const char *compat);
 
 int __of_add_property(struct device_node *np, struct property *prop);
+void __of_add_property_post(struct device_node *np, struct property *prop,
+		int ignore_dup_name);
 int __of_remove_property(struct device_node *np, struct property *prop);
+void __of_remove_property_post(struct device_node *np, struct property *prop);
 int __of_update_property(struct device_node *np, struct property *newprop,
 		struct property **oldprop);
+void __of_update_property_post(struct device_node *np, struct property *newprop,
+		struct property *oldprop);
 
 extern int of_add_property(struct device_node *np, struct property *prop);
 extern int of_remove_property(struct device_node *np, struct property *prop);
@@ -364,7 +369,9 @@ extern int of_reconfig_notifier_unregister(struct notifier_block *);
 extern int of_reconfig_notify(unsigned long, void *);
 
 void __of_attach_node(struct device_node *np);
+void __of_attach_node_post(struct device_node *np);
 void __of_detach_node(struct device_node *np);
+void __of_detach_node_post(struct device_node *np);
 
 extern int of_attach_node(struct device_node *);
 extern int of_detach_node(struct device_node *);
