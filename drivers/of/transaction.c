06388f4c025ee1049c65d0f72f81c414c36be7b6
Grant Likely <grant.likely@linaro.org>
2014-07-12 07:30:07 -0600

    fixup: implement of_transaction_entry_revert() using of_transaction_entry_apply()
    Apply and revert are identical operations as far as the rest of the
    kernel is concerned. They both modify the tree, and the operations
    required to modify the tree must proceed in exactly the same order.
    Instead of having a completely separate implementation for reverting
    changes, use of_transaction_entry_invert() to create a transaction entry
    that describes the opposite operation and use
    of_transaction_entry_apply() to process it.

--- drivers/of/transaction.c
+++ drivers/of/transaction.c
@@ -117,7 +117,7 @@ static int __of_transaction_entry_notify(struct of_transaction_entry *te, bool r
 
 static int __of_transaction_entry_apply(struct of_transaction_entry *te)
 {
-	struct property *old_prop;
+	struct property *old_prop, **propp;
 	unsigned long flags;
 	int ret = -EINVAL;
 
@@ -130,6 +130,15 @@ static int __of_transaction_entry_apply(struct of_transaction_entry *te)
 		__of_detach_node(te->np);
 		break;
 	case OF_RECONFIG_ADD_PROPERTY:
+		/* If the property is in deadprops then it must be removed */
+		for (propp = &te->np->deadprops; *propp; propp = &(*propp)->next) {
+			if (*propp == te->prop) {
+				*propp = te->prop->next;
+				te->prop->next = NULL;
+				break;
+			}
+		}
+
 		ret = __of_add_property(te->np, te->prop);
 		if (ret) {
 			pr_err("%s: add_property failed @%s/%s\n",
@@ -147,7 +156,17 @@ static int __of_transaction_entry_apply(struct of_transaction_entry *te)
 			break;
 		}
 		break;
+
 	case OF_RECONFIG_UPDATE_PROPERTY:
+		/* If the property is in deadprops then it must be removed */
+		for (propp = &te->np->deadprops; *propp; propp = &(*propp)->next) {
+			if (*propp == te->prop) {
+				*propp = te->prop->next;
+				te->prop->next = NULL;
+				break;
+			}
+		}
+
 		ret = __of_update_property(te->np, te->prop, &old_prop);
 		if (ret) {
 			pr_err("%s: update_property failed @%s/%s\n",
@@ -159,10 +178,8 @@ static int __of_transaction_entry_apply(struct of_transaction_entry *te)
 	}
 	raw_spin_unlock_irqrestore(&devtree_lock, flags);
 
-	if (ret) {
-		/* we can't rollback the notifier */
+	if (ret)
 		return ret;
-	}
 
 	switch (te->action) {
 	case OF_RECONFIG_ATTACH_NODE:
@@ -186,97 +203,12 @@ static int __of_transaction_entry_apply(struct of_transaction_entry *te)
 	return 0;
 }
 
-static int __of_transaction_entry_revert(struct of_transaction_entry *te)
+static inline int __of_transaction_entry_revert(struct of_transaction_entry *te)
 {
-	struct property *prop, *old_prop, **propp;
-	unsigned long action, flags;
-	struct device_node *np;
-	int ret = -EINVAL;
+	struct of_transaction_entry te_inverted;
 
-	/* get node and immediately put */
-	action = te->action;
-	np = te->np;
-	prop = te->prop;
-	old_prop = te->old_prop;
-
-	raw_spin_lock_irqsave(&devtree_lock, flags);
-	switch (action) {
-	case OF_RECONFIG_ATTACH_NODE:
-		__of_detach_node(np);
-		break;
-	case OF_RECONFIG_DETACH_NODE:
-		__of_attach_node(np);
-		break;
-	case OF_RECONFIG_ADD_PROPERTY:
-		ret = __of_remove_property(np, prop);
-		if (ret) {
-			pr_err("%s: remove_property failed @%s/%s\n",
-				__func__, np->full_name, prop->name);
-			break;
-		}
-		break;
-	case OF_RECONFIG_REMOVE_PROPERTY:
-	case OF_RECONFIG_UPDATE_PROPERTY:
-		/* property is *always* on deadprops */
-		if (action == OF_RECONFIG_UPDATE_PROPERTY)
-			prop = old_prop;
-		propp = &np->deadprops;
-		while (*propp != NULL) {
-			if (*propp == prop)
-				break;
-			propp = &(*propp)->next;
-		}
-
-		/* we should find it in deadprops */
-		WARN_ON(*propp == NULL);
-
-		/* remove it from deadprops */
-		if (*propp != NULL)
-			*propp = prop->next;
-
-		if (action == OF_RECONFIG_REMOVE_PROPERTY) {
-			ret = __of_add_property(np, prop);
-			if (ret) {
-				pr_err("%s: add_property failed @%s/%s\n",
-					__func__, np->full_name,
-					prop->name);
-				break;
-			}
-		} else {
-			ret = __of_update_property(np, prop, &old_prop);
-			if (ret) {
-				pr_err("%s: update_property failed @%s/%s\n",
-					__func__, np->full_name,
-					prop->name);
-				break;
-			}
-		}
-		break;
-	}
-	raw_spin_unlock_irqrestore(&devtree_lock, flags);
-
-	if (ret)
-		return ret;
-
-	switch (action) {
-	case OF_RECONFIG_ATTACH_NODE:
-		__of_detach_node_sysfs(np);
-		break;
-	case OF_RECONFIG_DETACH_NODE:
-		__of_attach_node_sysfs(np);
-		break;
-	case OF_RECONFIG_ADD_PROPERTY:
-		__of_remove_property_sysfs(np, prop);
-		break;
-	case OF_RECONFIG_REMOVE_PROPERTY:
-		__of_add_property_sysfs(np, prop);
-		break;
-	case OF_RECONFIG_UPDATE_PROPERTY:
-		__of_update_property_sysfs(np, prop, old_prop);
-		break;
-	}
-
-	return 0;
+	__of_transaction_entry_invert(te, &te_inverted);
+	return __of_transaction_entry_apply(&te_inverted);
 }
 
 /**
