f9f14e469bb135650808429b2a4847aac659cce6
Pantelis Antoniou <pantelis.antoniou@konsulko.com>
2014-07-28 22:38:53 +0300

    of: Add old prop argument on OF_RECONFIG_UPDATE_PROPERTY
    The notifier now includes the old_prop argument when updating
    properties, propagate this API to changeset internals while
    also retaining the old behaviour of retrieving the old_property
    when NULL is passed.
    
    Signed-off-by: Pantelis Antoniou <pantelis.antoniou@konsulko.com>

--- drivers/of/selftest.c
+++ drivers/of/selftest.c
@@ -428,7 +428,7 @@ static void __init of_selftest_changeset(void)
 	selftest(!of_changeset_detach_node(&chgset, nremove), "fail remove node\n");
 	selftest(!of_changeset_attach_node(&chgset, n21), "fail attach n21\n");
 	selftest(!of_changeset_add_property(&chgset, parent, ppadd), "fail add prop\n");
-	selftest(!of_changeset_update_property(&chgset, parent, ppupdate), "fail update prop\n");
+	selftest(!of_changeset_update_property(&chgset, parent, ppupdate, NULL), "fail update prop\n");
 	selftest(!of_changeset_remove_property(&chgset, parent, ppremove), "fail remove prop\n");
 	mutex_lock(&of_mutex);
 	selftest(!of_changeset_apply(&chgset), "apply failed\n");
