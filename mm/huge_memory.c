71924e734e160c4c8bf185d7fc5dc4bfd6b967e9
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2015-12-18 15:36:20 +1100

    thp: fix leak due split_huge_page() vs. exit race
    Consider following race:
    
    		CPU0				CPU1
    shrink_page_list()
      add_to_swap()
        split_huge_page_to_list()
          __split_huge_pmd_locked()
            pmdp_huge_clear_flush_notify()
    	// pmd_none() == true
    					exit_mmap()
    					  unmap_vmas()
    					    zap_pmd_range()
    					      // no action on pmd since pmd_none() == true
    	pmd_populate()
    
    As result the THP will not be freed. The leak is detected by check_mm():
    
    	BUG: Bad rss-counter state mm:ffff880058d2e580 idx:1 val:512
    
    The patch restore the logic original split_huge_page() had before
    refcounting rework: never have intermediate pmd_none() == true.
    
    There are few other places where we do have pmd_none() == true for some
    time, but they are safe:
    
     - __split_huge_zero_page_pmd() is not reachable during exit, since huge
       zero page is not on LRU.
    
     - do_huge_pmd_wp_page() and do_huge_pmd_wp_page_fallback() are also not
       reachable during exit: exit_mmap() and handling page fault for the mm
       are mutual exclusive.
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -2802,9 +2802,6 @@ static void __split_huge_pmd_locked(struct vm_area_struct *vma, pmd_t *pmd,
 	write = pmd_write(*pmd);
 	young = pmd_young(*pmd);
 
-	/* leave pmd empty until pte is filled */
-	pmdp_huge_clear_flush_notify(vma, haddr, pmd);
-
 	pgtable = pgtable_trans_huge_withdraw(mm, pmd);
 	pmd_populate(mm, &_pmd, pgtable);
 
@@ -2854,6 +2851,28 @@ static void __split_huge_pmd_locked(struct vm_area_struct *vma, pmd_t *pmd,
 	}
 
 	smp_wmb(); /* make pte visible before pmd */
+	/*
+	 * Up to this point the pmd is present and huge and userland has the
+	 * whole access to the hugepage during the split (which happens in
+	 * place). If we overwrite the pmd with the not-huge version pointing
+	 * to the pte here (which of course we could if all CPUs were bug
+	 * free), userland could trigger a small page size TLB miss on the
+	 * small sized TLB while the hugepage TLB entry is still established in
+	 * the huge TLB. Some CPU doesn't like that.
+	 * See http://support.amd.com/us/Processor_TechDocs/41322.pdf, Erratum
+	 * 383 on page 93. Intel should be safe but is also warns that it's
+	 * only safe if the permission and cache attributes of the two entries
+	 * loaded in the two TLB is identical (which should be the case here).
+	 * But it is generally safer to never allow small and huge TLB entries
+	 * for the same virtual address to be loaded simultaneously. So instead
+	 * of doing "pmd_populate(); flush_pmd_tlb_range();" we first mark the
+	 * current pmd notpresent (atomically because here the pmd_trans_huge
+	 * and pmd_trans_splitting must remain set at all times on the pmd
+	 * until the split is complete for this pmd), then we flush the SMP TLB
+	 * and finally we write the non-huge version of the pmd entry with
+	 * pmd_populate.
+	 */
+	pmdp_invalidate(vma, haddr, pmd);
 	pmd_populate(mm, pmd, pgtable);
 
 	if (freeze) {
